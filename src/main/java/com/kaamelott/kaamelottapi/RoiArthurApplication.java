f6444d2d8bd8db24c8d81bc6420c84ebfed4da0c
Gab <gabfer258@gmail.com>
2025-07-03 09:22:48 +0200

    Initial commit

---
+++ src/main/java/com/kaamelott/kaamelottapi/RoiArthurApplication.java
@@ -0,0 +1,13 @@
+package com.kaamelott.kaamelottapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoiArthurApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoiArthurApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kaamelott/kaamelottapi/RoiArthurApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaamelott.kaamelottapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoiArthurApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
